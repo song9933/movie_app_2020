f29b976a74c829b32fc58268e006aa0d71d39ab8
Your Name <sngbrm99@gmail.com>
2020-04-23 16:21:59 +0900

    #3.2 Component life cycle 20/04/23

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-function Food({name, picture}){
-  return <div>
-    <h2>I like {name}</h2>
-    <img src={picture} />
-  </div>
-}
-
-const foodIlike=[
-  { 
-    name : "macaron" ,
-    image : "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2F20120827_34%2Fbumgoo02_1346056898172fdOOE_JPEG%2Ftumblr_m8fxlefy6G1qzipvbo1_500_large.jpg&type=b400"
-  },
-  { 
-    name : "coffee" ,
-    image : "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2F20141008_98%2Fclean7793_1412746298639itoiH_GIF%2F2.gif&type=b400"
-  },
-  { 
-    name : "croffle",
-    image : "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMDAxMDhfNzUg%2FMDAxNTc4NDkwNDcwODk2.dSKZahdbCVju6CiswYC6mci7OIdcp6sxt34v6BHEBLcg.nlTR4LYKNLeyMFMqZD1L6oGcc4tTu0-JuP4fzc212CIg.JPEG.catousa%2F%25BE%25C6%25BF%25EC%25C7%25C1%25B1%25DB%25B7%25BF_%25283%2529.jpg&type=b400"
+class App extends React.Component{
+  state = {
+    //state는 object 이고, component의 data를 공간이있고 이 데이터는 변한다
+    count : 0
+  };
+  constructor(props){
+    super(props);
+    console.log("hello");
   }
-];
-
-function App() {
+  componentDidMount(){
+    console.log("component rendered");
+  }
+  componentDidUpdate(){
+    console.log("I just updated")
+  }
+  
+  add = () => {
+    this.setState(current =>({count: current.count +1}));
+  };
+  minus = () => {
+    this.setState(current =>({count: current.count -1}));
+  };
+  render(){
+    console.log("i am rendering!");
   return (
     <div>
-      <h1>Hello</h1>
-      {foodIlike.map(dish =>(
-        <Food name={dish.name} picture={dish.image}/>
-      ))}
-    </div>
-  );
+      <h1>the number is :  {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+    </div>);
+  }
 }
 
 export default App;
