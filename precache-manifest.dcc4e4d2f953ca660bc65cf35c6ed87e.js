2f44949b2d1b5c5b5fa371d84bd73d2aee93394d
Your Name <sngbrm99@gmail.com>
2020-04-27 12:07:30 +0900

    Updates

---
+++ precache-manifest.dcc4e4d2f953ca660bc65cf35c6ed87e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d694cf8c53597208699667cbaffcbd6f",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "842da9562232f914d85e",
+    "url": "/movie_app_2020/static/css/main.ac59167b.chunk.css"
+  },
+  {
+    "revision": "08f6efee45481d1ea76e",
+    "url": "/movie_app_2020/static/js/2.19d622a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.19d622a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "842da9562232f914d85e",
+    "url": "/movie_app_2020/static/js/main.9d5ffdff.chunk.js"
+  },
+  {
+    "revision": "e76fc168c1d893da3cab",
+    "url": "/movie_app_2020/static/js/runtime-main.ea46b0e5.js"
+  }
+]);
